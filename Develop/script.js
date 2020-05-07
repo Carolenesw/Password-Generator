1848d78c4ab3a5d7d90fc59b493a1bb1c5edabbf
Carolene Wilson <carolenesw@gmail.com>
2020-05-07 14:41:39 -0400

    create variable and prompts for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
-// Assignment Code
+// Create required variables based on password criteria
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", "(\)", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?",];
+
+// create prompts for password selection
+var userSelection = prompt("Choose password criteria: lowercase, uppercase, numbers or special character?");
+
+var passLenght = prompt("Please choose a password lenght between 8 and 128");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

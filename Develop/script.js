bef9031a690002d6ae27635978550045b7aa7895
Carolene Wilson <carolenesw@gmail.com>
2020-05-08 14:28:10 -0400

    correct typo error prompt alerts

--- Develop/script.js
+++ Develop/script.js
@@ -8,25 +8,26 @@ var specialChar = "`~!@<#>$?%^&.*,-_=+{[]}|\);:'";
 //how do i add " and /
 var charSel = "";
 var passLenght = "";
-var userPassword = "";
-
+var userPassword = [];
 
 // var userSelection = prompt("Choose password criteria: lowercase, uppercase, numbers or special character?");
 
 // create function to generate password based on user selction 
 function generatePassword() {
   var charSel = "";
-  var userPassword = "";
+  var userPassword = [];
 
   // user password selection criteria 
   var passLenght = Number(prompt("Please choose a password lenght between 8 and 128"));
 
   if (passLenght >= 8 && passLenght <=128){
-    lowerCaseSel = prompt(("If lowercase character in password. Yes (y) or No (n)")).toLowerCase;
-    upperCaseSel = prompt(("If uppercase character password. Yes (y) or No (n)")).toLowerCase;
-    numSel = prompt(("If numbers character password. Yes (y) or No (n)")).toLowerCase;
-    specialCharSel = prompt(("If special characters in password. Yes (y) or No (n)")).toLowerCase;
+    lowerCaseSel = (prompt("If lowercase character in password? Yes (y) or No (n)")).toLowerCase();
+    upperCaseSel = (prompt("If uppercase character password? Yes (y) or No (n)")).toLowerCase();
+    numSel = (prompt("If numbers character password? Yes (y) or No (n)")).toLowerCase();
+    specialCharSel = (prompt("If special characters in password? Yes (y) or No (n)")).toLowerCase();
 
+
+    // lowercaseValue = (prompt("Do you want lowercase characters in password? Yes (y) or No (n)")).toLowerCase();  
 // error message will display is password is less than 8 or greater than 128
   } else if (passLenght < 8 || passLenght > 128) {
     alert("Error, please enter a number between 8 and 128");
@@ -34,23 +35,36 @@ function generatePassword() {
   }
 
 //if the user selection is yes then add to character criteria option
-  if (lowerCaseSel === "y") {
-    charSel += lowercase;
+  if (lowerCaseSel == "y") {
+    charSel =+ lowerCase
+    lowerCase.push(userPassword)
   }
-  if (upperCaseSel === "y") {
-    charSel += upperCase;
+  if (upperCaseSel == "y") {
+    charSel =+ upperCase
+    upperCase.push[userPassword]
   }
-  if (numSel === "y") {
-    charSel += num;
+  if (numSel.value == "y") {
+    charSel =+ num
+    num.push[userPassword]
   }
-  if (specialCharSel === "y") {
-    charSel += specialChar;
+  if (specialCharSel == "y") {
+    charSel =+ specialChar
+    specialChar.push[userPassword]
   }
  
+  console.log(charSel)
+  console.log(userPassword)
+  console.log (passLenght)
+
   // create an alert message if no character set is selected, prompting user to make selection 
-  else if (charSel === "") {
+  if (charSel == "") {
     alert("Please select at least one character set!");
-    generatePassword()
+    
+  } else {
+    // for (var i = 0; i < passLenght; i++) {
+    //   // userPassword += charSel.charAt(Math.floor(Math.random() * charSel.length))
+    //   userPassword(Math.floor(Math.random () * passLenght))
+    // }
   }
  
 

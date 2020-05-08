b1f2a8af206cb2bfbd7dc18b48262add5e0b901a
Carolene Wilson <carolenesw@gmail.com>
2020-05-08 12:20:23 -0400

    alert user if no character is selected

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,8 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
-// var specialChar = "`~!@#$%^&*,-_=+{[]}|\);:'"<>./-?";
-
+var specialChar = "`~!@<#>$?%^&.*,-_=+{[]}|\);:'";
+//how do i add " and /
 var charSel = "";
 var passLenght = "";
 var userPassword = "";
@@ -29,14 +29,32 @@ function generatePassword() {
 
 // error message will display is password is less than 8 or greater than 128
   } else if (passLenght < 8 || passLenght > 128) {
-    alert("Error, please enter a number between 8 and 128")
+    alert("Error, please enter a number between 8 and 128");
+    generatePassword()
   }
 
-
-
-  }  
-
+//if the user selection is yes then add to character criteria option
+  if (lowerCaseSel === "y") {
+    charSel += lowercase;
+  }
+  if (upperCaseSel === "y") {
+    charSel += upperCase;
+  }
+  if (numSel === "y") {
+    charSel += num;
+  }
+  if (specialCharSel === "y") {
+    charSel += specialChar;
+  }
  
+  // create an alert message if no character set is selected, prompting user to make selection 
+  else if (charSel === "") {
+    alert("Please select at least one character set!");
+    generatePassword()
+  }
+ 
+
+}
 
   
 
